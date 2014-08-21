ec2ca5908f2200d651f9aeb6be453f87563f816b
pramod <pramod@tavisca.com>
2014-08-21 09:51:30 +0530

    Added basic model of the assignment

---
+++ MvcMovie/Migrations/201408200935376_AddPriceDataAnnotationsMig.cs
@@ -0,0 +1,16 @@
+namespace MvcMovie.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddPriceDataAnnotationsMig : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
